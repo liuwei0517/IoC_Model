4016ffe3fe682b0de2b27cd0bda7ae3db58c59e1
liuweifeng <faithmo@yeah.net>
2019-01-21 15:21:59 +0800

    依赖反转

---
+++ KoalaInterFaceBLL/BG_RequestMTS_FileInfo_Detaile_BLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoalaInterFaceBLL
+{
+    public class BG_RequestMTS_FileInfo_Detaile_BLL<T> : BaseBLL<T> where T:class
+    {
+        public override T Get(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KoalaInterFaceBLL/BaseBLL.cs
@@ -0,0 +1,19 @@
+﻿using KoalaInterFaceModels;
+using KoalaInterFaceService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoalaInterFaceBLL
+{
+    public abstract class BaseBLL<T> : IRepository<T> where T : class
+    {
+        public KoalaKnowDevEntities GetDbContext => throw new NotImplementedException();
+
+        public abstract T Get(System.Linq.Expressions.Expression<Func<T, bool>> predicate);
+        public abstract List<T> GetList(Expression<Func<T, bool>> expression);
+    }
+}
